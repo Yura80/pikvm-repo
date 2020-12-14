e2bdef8e536524a15b721b0b48e8620e8910e560
yura <yura407@gmail.com>
2020-12-13 20:10:59 -0500

    repo update

--- commit.sh
+++ commit.sh
@@ -2,6 +2,8 @@ cp -a ../packages/repos/* .
 for f in $(find -type l); do 
     cp --remove-destination `readlink -f "$f"` "$f"
 done
+rm -rf rpi3-arm
+cp -a rpi2-arm rpi3-arm
 git add --all
 git commit -m "repo update"
 git push
